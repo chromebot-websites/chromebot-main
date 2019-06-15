88c0adc98d96a4f50de1fd983c1e012a853a7464
Restyled.io <commits@restyled.io>
2019-06-15 19:20:11 +0000

    Restyled by prettier

--- client/index.js
+++ client/index.js
@@ -1,31 +1,39 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 let buildNumber = 0.24;
 
-console.log("Welcome to the chromebot website. This is client build B." + buildNumber + ".");
-
+console.log(
+  "Welcome to the chromebot website. This is client build B." +
+    buildNumber +
+    "."
+);
 
 class App extends Component {
-	render() {
-		return (
-			<Router>
-				<Switch>
-					<Route path="/status" render={() => {
-						return (
-							<iframe src="https://chromebot-status-page-beta-114.freshstatus.io/"/>
-						);
-					}}/>
-					<Route render={() => {
-						return (
-							<iframe src="https://chromebot-canary-and-chromebot.weebly.com/"/>
-						);
-					}}/>
-				</Switch>
-			</Router>
-		);
-	}
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route
+            path="/status"
+            render={() => {
+              return (
+                <iframe src="https://chromebot-status-page-beta-114.freshstatus.io/" />
+              );
+            }}
+          />
+          <Route
+            render={() => {
+              return (
+                <iframe src="https://chromebot-canary-and-chromebot.weebly.com/" />
+              );
+            }}
+          />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
